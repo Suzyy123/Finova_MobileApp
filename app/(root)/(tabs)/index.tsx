10967c69f49c8465d4ed5028010c9f27c79ca6a0
suzu <sankharsujita@gmail.com>
2026-09-05 23:26:09 +0545

    Added Financial Overview Section

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,5 +1,6 @@
 import { Ionicons } from "@expo/vector-icons";
 import { LinearGradient } from "expo-linear-gradient";
+import { useState } from "react";
 import { FlatList, ScrollView, Text, TouchableOpacity, View } from "react-native";
 //Financial Overview
 const financialOverviewData = [
@@ -7,8 +8,7 @@ const financialOverviewData = [
     "id": "1",
     "title" : "Gross Revenue",
     "value" : "$7.7K",
-    "icon" : "trending-up-outline",
-    "color" : "#4ADE80",
+    "icon" : "arrow-up-outline",
     "num" : "100.0%"
 
   },
@@ -16,26 +16,27 @@ const financialOverviewData = [
     "id": "2",
     "title" : "Net Profit",
     "value" : "$2.3K",
-    "icon" : "trending-up-outline",
-    "color" : "#3B82F6",
+    "icon" : "arrow-up-outline",
     "num" : "30.0%"
   },
   {
     "id": "3",
     "title" : "Expenses",
     "value" : "$5.48K",
-    "icon" : "trending-down-outline",
-    "color" : "#F87171",
+    "icon" : "arrow-down-outline",
     "num" : "70.0%"
   }
 
 ];
+
 export default function Index() {
+//For color changing  of the button
+const [selectedPeriod, setSelectedPeriod] = useState("Week");
   return (
-    <View className="flex-1 bg-white">
+    <View className="flex-1 bg-gray-100">
       {/* Heading */}
       <LinearGradient
-        colors={["#F1EBFF", "#FCEFFF", "#FEE8D6"]}
+        colors={["#F1EBFF", "#FCEFFF", "#FCE4EC"]}
         start={{ x: 0, y: 0 }}
         end={{ x: 1, y: 1 }}
         className="h-60"
@@ -64,7 +65,7 @@ export default function Index() {
       </LinearGradient>
 
       {/* Financial Overview */}
-      <View className="flex-column px-4 py-2 mt-4">
+      <View className="px-4 py-2 mt-4">
         <Text className="text-lg font-semibold">Financial Overview</Text>
         {/* Cards */}
         <ScrollView
@@ -76,15 +77,66 @@ export default function Index() {
             showsHorizontalScrollIndicator={false}  
             keyExtractor={(item)=> item.id}
             renderItem={({item})=>(
-              <View className="bg-red-100 px-4 py-3 rounded-2xl mr-4 shadow-md">
-                <Text className="font-bold">{item.title}</Text>
+              <View 
+              className="bg-white px-4 py-3 rounded-2xl mr-4 shadow-lg mt-4 items-start w-40 h-30">
+                <Text className="text-gray-500">{item.title}</Text>
+                <Text className="text-black text-xl font-bold mt-2 ">{item.value}</Text>
+                <View className="flex-row items-center mt-2 bg-green-100 rounded-2xl w-24 h-6">
+                  <View className="rotate-45 px-2">
+                  <Ionicons
+                  name={item.icon as any}
+                  size={18} 
+                  color={item.icon === "arrow-up-outline" ? "green" : "red"}
+                />
+                </View>
+                  <Text className="text-xs text-green-700">{item.num}</Text>
+                </View>
               </View>
-            )}
-          
-          
+            )}  
           />
         </ScrollView>
+          {/* Activity  */}
+        <View className="mt-2 flex-row">
+        <Text className="text-lg font-semibold py-2.5">Activity</Text>
+        <View className="flex-row ml-auto border border-gray-300 rounded-3xl items-center ">
+        {/* Week */}
+        <TouchableOpacity
+        onPress={() => setSelectedPeriod("Week")}
+        className={`rounded-2xl px-2 py-2 ${
+        selectedPeriod === "Week" ? "bg-white " : ""
+        }`}
+        >
+        <Text className={`text-sm ${
+          selectedPeriod === "Week" ? "text-purple-600" : "text-gray-600"
+        }`}>Week</Text>
+        </TouchableOpacity>
+        {/* Day */}
+        <TouchableOpacity
+        onPress={() => setSelectedPeriod("Day")}
+        className={`rounded-2xl px-3 py-2 ${
+        selectedPeriod === "Day" ? "bg-white text-purple-500" : ""
+        }`}
+        >
+        <Text className={`text-sm ${
+          selectedPeriod === "Day" ? "text-purple-600" : "text-gray-600"
+        }`}>Day</Text>
+        </TouchableOpacity>
+        {/* Month */}
+        <TouchableOpacity
+        onPress={() => setSelectedPeriod("Month")}
+        className={`rounded-2xl px-2 py-2 ${
+        selectedPeriod === "Month" ? "bg-white text-purple-500" : ""
+        }`}
+        >
+          <Text className={`text-sm ${
+            selectedPeriod === "Month" ? "text-purple-600" : "text-gray-600"
+          }`}>Month</Text>
+        </TouchableOpacity>
+        </View>
       </View>
+      </View>
+    
+      
     </View>
   );
 }
